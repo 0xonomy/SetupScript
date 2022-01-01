369dc9f5f023ed2ba8b277522460bd18470a44a5
G0ldenJ0ker <79750374+G0ldenJ0ker@users.noreply.github.com>
2022-01-01 19:12:42 +0400

    Update GJ-setup.sh

--- GJ-setup.sh
+++ GJ-setup.sh
@@ -5,56 +5,56 @@
 #Script for automaticly downloading and installing/compiling my customized stuff: dwm, st and etc.
 
 
-echo("Installing dependencies...")
+echo "Installing dependencies..."
 sudo apt install git libx11-dev debhelper bison check cmake flex ghostscript libbsd-dev libcurl4-openssl-dev libgeoip-dev libltdl-dev libluajit-5.1-dev libncurses5-dev libnet1-dev libpcap-dev libpcre3-dev libssl-dev libgtk-3-dev libgtk2.0-dev compton nitrogen unzip wget surf scrot -y
 
 git clone https://github.com/G0ldenJ0ker/src.git ~/src/
 
-echo("compiling dwm...")
+echo "compiling dwm..."
 cd ~/src/dwm/
 sudo make clean install
 sudo cp ~/src/dwm.desktop /usr/share/xsessions/
 
 
-echo("compiling dwmblocks...")
+echo "compiling dwmblocks..."
 cd ~/src/dwmblocks/
 sudo make clean install
 
 
-echo("compiling dmenu...")
+echo "compiling dmenu..."
 cd ~/src/dmenu/
 sudo make clean install
 
 
-echo("compiling st...")
+echo "compiling st..."
 cd ~/src/st/
 sudo make clean install
 
 
-echo("Setting up autostart script...")
+echo "Setting up autostart script..."
 mkdir ~/.dwm/
 cp ~/src/autostart.sh ~/.dwm/
 
 
-echo("Setting up other scripts...")
+echo "Setting up other scripts..."
 chmod +x ~/src/scripts/*
 sudo cp ~/src/scripts/* /bin/
 
 
-echo("Setting up fonts...")
+echo "Setting up fonts..."
 sudo mkdir /usr/share/fonts/JetBrainsMono/
 sudo cp ~/src/fonts/JetBrainsMono.zip /usr/share/fonts/JetBrainsMono/
 sudo cd /usr/share/fonts/JetBrainsMono/
 sudo unzip JetBrainsMono.zip
 
 
-echo("Setting up wallpapers...")
+echo "Setting up wallpapers..."
 mkdir ~/Pictures/
 mkdir ~/Pictures/wallpapers/
 cp ~/src/wallpapers/* ~/Pictures/wallpapers/
 
 
-echo("Setting up bash...")
+echo "Setting up bash..."
 mv ~/.bashrc ~/.bashrc.old
 cp ~/src/bashrc ~/.bashrc
 
